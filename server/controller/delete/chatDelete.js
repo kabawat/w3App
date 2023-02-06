70c1ddd81ddb0be16376953d6d44c45b1450146d
w3App <kshatriyakabawat@gmail.com>
2023-02-06 21:50:37 +0530

    delete chat API Error handling

--- server/controller/delete/chatDelete.js
+++ server/controller/delete/chatDelete.js
@@ -1,9 +1,11 @@
 const { chatModal } = require('../connection')
-exports.deleteChat = (req, res) => {
-    const getOperation = async () => {
-        const { chat_id } = req.query
-        const result = await chatModal.deleteOne({ chat_id })
-        res.send(`delete Chat ${result.deletedCount}`)
+exports.deleteChat = async (req, res) => {
+    try {
+        const { chat_id } = req.query;
+        const result = await chatModal.deleteOne({ chat_id });
+        res.send(`delete Chat ${result.deletedCount}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Something went wrong while deleting the chat' });
     }
-    getOperation()
-}
+};
