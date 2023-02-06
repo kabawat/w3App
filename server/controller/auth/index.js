147905307b3f8f0799b512a0acd58d5ff0e01d8e
w3App <kshatriyakabawat@gmail.com>
2023-02-06 21:43:48 +0530

    sign-up and user authentication verify
    username and email required fields,
    and
    user verification API

--- server/controller/auth/index.js
+++ server/controller/auth/index.js
@@ -1,50 +1,53 @@
 const { userModal } = require('../connection')
 const jwt = require('jsonwebtoken')
-exports.verify = (req, res, next) => {
-    const token = req.headers.authorization
+exports.verify = async (req, res, next) => {
     try {
-        if (token) {
-            const isValidUser = jwt.verify(token, process.env.PRIVETKEY)
-            if (isValidUser) {
-                userModal.findOne({ user: isValidUser.user }).then((result) => {
-                    next()
-                })
-            } else {
-                throw false
-            }
-        } else {
-            res.status(401).json({
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({
                 status: false,
-                massage: 'invalid authentication credentials for the requested resource',
-            })
+                message: "invalid authentication credentials for the requested resource"
+            });
         }
-    } catch (error) {
-        res.status(401).json({
-            status: false,
-            massage: 'invalid authentication credentials for the requested resource',
-        })
-    }
-}
 
-exports.userVerify = (req, res) => {
-    const requestToken = req.headers.authorization
-    const { user, email } = jwt.decode(requestToken)
-    const token = jwt.sign({ user, email }, process.env.PRIVETKEY)
-    try {
-        if (token) {
-            res.status(200).json({
-                massage: 'success',
-                status: true,
-                status_code: 200,
-                token
-            })
-        } else {
-            throw false
+        const isValidUser = jwt.verify(token, process.env.PRIVETKEY);
+        if (!isValidUser) {
+            throw new Error("invalid authentication credentials for the requested resource");
         }
+
+        const user = await userModal.findOne({ user: isValidUser.user });
+        if (!user) {
+            throw new Error("invalid authentication credentials for the requested resource");
+        }
+
+        next();
     } catch (error) {
-        res.status(501).json({
-            massage: 'internal server error',
+        return res.status(401).json({
             status: false,
-        })
+            message: error.message
+        });
+    }
+};
+exports.userVerify = async (req, res) => {
+    try {
+        const requestToken = req.headers.authorization;
+        const decoded = jwt.decode(requestToken);
+        const { user, email } = decoded;
+        const token = jwt.sign({ user, email }, process.env.PRIVETKEY);
+        if (!token) {
+            throw new Error('Failed to generate token');
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            status_code: 200,
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(501).json({
+            message: 'internal server error',
+            status: false,
+        });
     }
 }
--- server/controller/signup/index.js
+++ server/controller/signup/index.js
@@ -2,32 +2,37 @@ const { userModal } = require('../connection')
 exports.singUpValidation = async (req, res, next) => {
     try {
         const { user, email } = req.body
-        const isEmail = await userModal.findOne({ email })
-        const isUser = await userModal.findOne({ user })
+        if (!user || !email) {
+            res.status(400).json({
+                status: false,
+                massage: 'User and email are required fields'
+            });
+            return;
+        }
+        const isEmail = await userModal.findOne({ email });
+        const isUser = await userModal.findOne({ user });
         if (isEmail) {
             res.status(409).json({
                 status: false,
                 massage: 'Email id already exists'
-            })
-            return
+            });
+            return;
         }
         if (isUser) {
             res.status(409).json({
                 status: false,
                 massage: 'User id already exists'
-            })
-            return
+            });
+            return;
         }
-        if (!isEmail && !isUser) {
-            next()
-        }
+        next();
     } catch (error) {
         res.status(500).json({
             status: false,
             massage: 'Something went wrong. Please try again later'
-        })
+        });
     }
-}
+};
 exports.Signup = async (req, res) => {
     try {
         const { user, email, pwd } = req.body
